575c4fe26741295a26da3e7e8124ce3ce19d2c0b
rafael aurichi <rafael.aurichi@ouzzi.ag>
2023-04-12 17:58:11 -0300

    funcoes deposito, saque e transferencia

---
+++ ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public int numAgecia;
+        public string numConta;
+        public string titular;
+        public double saldo;
+
+        public void Depositar(double deposito)
+        {
+            this.saldo += deposito;
+        }
+
+        public bool Sacar(double saque)
+        {
+            if (this.saldo >= saque) 
+            { 
+                this.saldo -= saque;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double transf, ContaCorrente destino)
+        {
+            if (this.saldo >= transf)
+            {
+                this.Sacar(transf);
+                destino.Depositar(transf);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using ByteBank;
+
+ContaCorrente contaAndre = new ContaCorrente();
+contaAndre.titular = "André Silva";
+contaAndre.numConta = "1010-X";
+contaAndre.numAgecia = 15;
+contaAndre.saldo = 100;
+
+ContaCorrente contaMaria = new ContaCorrente();
+contaMaria.titular = "Maria Souza";
+contaMaria.numConta = "1010-5";
+contaMaria.numAgecia = 17;
+contaMaria.saldo = 350;
+
+Console.WriteLine("O saldo da conta do " + contaAndre.titular + " é: R$" + contaAndre.saldo + "\n");
+
+Console.WriteLine("O saldo da conta do " + contaMaria.titular + " é: R$" + contaMaria.saldo + "\n");
+
+if (contaAndre.Transferir(50, contaMaria) == true)
+{
+    Console.WriteLine("Transferência realizada com sucesso!\n");
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para realizar a transferência!\n");
+}
+
+Console.WriteLine("O saldo da conta do " + contaAndre.titular + " é: R$" + contaAndre.saldo + "\n");
+
+Console.WriteLine("O saldo da conta do " + contaMaria.titular + " é: R$" + contaMaria.saldo + "\n");
+
+/*
+contaAndre.Depositar(100);
+Console.WriteLine("O saldo da conta do " + contaAndre.titular + " após o depósito é: R$" + contaAndre.saldo + "\n");
+*/
+
+/*
+if (contaAndre.Sacar(150) == true)
+{
+    Console.WriteLine("Saque realizado com sucesso!");
+    Console.WriteLine("O saldo da conta do " + contaAndre.titular + " após o saque é: R$" + contaAndre.saldo + "\n");
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para realizar o saque!");
+}
+*/
+
+
+
+Console.ReadLine();
