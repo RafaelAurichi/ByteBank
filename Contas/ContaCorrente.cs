963fe77a3bf7feaa8656c937002e4ec3c06c45f0
rafael aurichi <rafael.aurichi@ouzzi.ag>
2023-04-15 20:57:39 -0300

    getters e setters

---
+++ Contas/ContaCorrente.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using ByteBank.Titular;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ByteBank.Contas
+{
+    public class ContaCorrente
+    {
+        public string NumConta { get; set; }
+        public Cliente Titular { get; set; }
+
+        private int _numAgencia;
+        public int NumAgencia
+        {
+            get { return this._numAgencia; }
+            set {
+                if (value > 0){
+                    this._numAgencia = value;
+                }
+            }
+        }
+
+        private double _saldo;
+        public double Saldo
+        {
+            get { return this._saldo; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error: Não é possível definir um valor negativo para o campo saldo.");
+                }
+                else
+                {
+                    this._saldo = value;
+                }
+            }
+        }
+
+
+        //MÉTODOS---------------------------------------------------
+        public void Depositar(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public bool Sacar(double saque)
+        {
+            if (Saldo >= saque)
+            {
+                Saldo -= saque;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double transf, ContaCorrente destino)
+        {
+            if (Saldo >= transf)
+            {
+                Sacar(transf);
+                destino.Depositar(transf);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,27 @@
-﻿using ByteBank;
+﻿using ByteBank.Contas;
+using ByteBank.Titular;
 
 ContaCorrente contaAndre = new ContaCorrente();
-contaAndre.titular = "André Silva";
-contaAndre.numConta = "1010-X";
-contaAndre.numAgecia = 15;
-contaAndre.saldo = 100;
+contaAndre.Titular = new Cliente();
+contaAndre.Titular.Nome = "André Silva";
+contaAndre.Titular.Cpf = "111.111.111-11";
+contaAndre.Titular.Profissao = "Desenvolvedor";
+contaAndre.NumConta = "1010-X";
+contaAndre.NumAgencia = 15;
+contaAndre.Saldo = 100;
 
 ContaCorrente contaMaria = new ContaCorrente();
-contaMaria.titular = "Maria Souza";
-contaMaria.numConta = "1010-5";
-contaMaria.numAgecia = 17;
-contaMaria.saldo = 350;
+contaMaria.Titular = new Cliente();
+contaMaria.Titular.Nome = "Maria Souza";
+contaMaria.Titular.Cpf = "222.222.222-22";
+contaMaria.Titular.Profissao = "Desenvolvedora";
+contaMaria.NumConta = "1010-5";
+contaMaria.NumAgencia = 17;
+contaMaria.Saldo = 350;
 
-Console.WriteLine("O saldo da conta do " + contaAndre.titular + " é: R$" + contaAndre.saldo + "\n");
+Console.WriteLine("O saldo da conta do " + contaAndre.Titular.Nome + " é: R$" + contaAndre.Saldo + "\n");
 
-Console.WriteLine("O saldo da conta do " + contaMaria.titular + " é: R$" + contaMaria.saldo + "\n");
+Console.WriteLine("O saldo da conta do " + contaMaria.Titular.Nome + " é: R$" + contaMaria.Saldo + "\n");
 
 if (contaAndre.Transferir(50, contaMaria) == true)
 {
@@ -25,26 +32,28 @@ else
     Console.WriteLine("Saldo insuficiente para realizar a transferência!\n");
 }
 
-Console.WriteLine("O saldo da conta do " + contaAndre.titular + " é: R$" + contaAndre.saldo + "\n");
+Console.WriteLine("O saldo da conta do " + contaAndre.Titular.Nome + " é: R$" + contaAndre.Saldo + "\n");
 
-Console.WriteLine("O saldo da conta do " + contaMaria.titular + " é: R$" + contaMaria.saldo + "\n");
+Console.WriteLine("O saldo da conta do " + contaMaria.Titular.Nome + " é: R$" + contaMaria.Saldo + "\n");
 
-/*
+
 contaAndre.Depositar(100);
-Console.WriteLine("O saldo da conta do " + contaAndre.titular + " após o depósito é: R$" + contaAndre.saldo + "\n");
-*/
+Console.WriteLine("O saldo da conta do " + contaAndre.Titular.Nome + " após o depósito é: R$" + contaAndre.Saldo + "\n");
 
-/*
+
+
 if (contaAndre.Sacar(150) == true)
 {
     Console.WriteLine("Saque realizado com sucesso!");
-    Console.WriteLine("O saldo da conta do " + contaAndre.titular + " após o saque é: R$" + contaAndre.saldo + "\n");
+    Console.WriteLine("O saldo da conta do " + contaAndre.Titular.Nome + " após o saque é: R$" + contaAndre.Saldo + "\n");
 }
 else
 {
     Console.WriteLine("Saldo insuficiente para realizar o saque!");
 }
-*/
+
+contaAndre.Saldo = -10;
+Console.WriteLine("O saldo da conta do " + contaAndre.Titular.Nome + " é: R$" + contaAndre.Saldo + "\n");
 
 
 
